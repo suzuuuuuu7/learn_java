dc3a96b2c7cf1188cf7c699f59f6a8d62272f7d2
suzuuuuuu7 <sujalkc324@gmail.com>
2026-04-06 12:50:27 +0545

    exception

---
+++ src/EXCT.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Optional;
+import java.util.Scanner;
+public class EXCT {
+    public static void main(String[] args){
+        try {
+            int a = 0;
+            int b = 0;
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter first value");
+                a = sc.nextInt();
+                System.out.println("Enter second value");
+                b = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println(e);
+                System.out.println("Plz inter valid integer");
+            }
+
+             int div = a/b;
+            System.out.println("Result" + div);
+        }
+        catch (Exception e){
+            System.out.println(e);
+            System.out.println("divide by one is undefined");
+        }
+        finally {
+            System.out.println("Iam always executed");
+        }
+
+    }
+}
